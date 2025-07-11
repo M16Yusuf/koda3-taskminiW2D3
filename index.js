2bc020346a3d80e83310d85a8eb8adab8a79d8f4
M16yusuf <yusufsmd58@gmail.com>
2025-07-12 06:46:23 +0700

    feat: add feature cetakTabel,mintask-w2d3no5

--- index.js
+++ index.js
@@ -53,6 +53,7 @@ const { myTriangle } = require("./tasks/yusuf/triangle.js");
 const { useCountArray } = require("./tasks/yusuf/countArray.js");
 const { myTriangleVer2 } = require("./tasks/yusuf/triangleVer2.js");
 const { prosesNumbers } = require("./tasks/yusuf/prosesNumber.js");
+const { cetakTabel } =require("./tasks/yusuf/cetakTabel.js");
 // panggil nomor 1 
 console.log(myTriangle(5));
 // panggil nomor 2 dan asign nilai
@@ -61,5 +62,7 @@ console.log(useCountArray([1,4,5,6,4,6])); // hasil = 2880
 console.log(myTriangleVer2(5));
 // panggil nomor 4
 console.log(prosesNumbers([1,2,3,4,5])); // hasil 15
+// panggil nomor 5
+console.log(cetakTabel(5));
 
 
---
+++ tasks/yusuf/cetakTabel.js
@@ -0,0 +1,22 @@
+/**
+ * Buat sebuah fungsi bernama cetakTabel yang menerima satu
+ * parameter angka n. Fungsi ini harus mencetak tabel perkalian
+ * untuk angka n dari 1 sampai 10
+ */
+
+const cetakTabel = function(inputAngka){
+    let hasil = ""; 
+    if(typeof inputAngka != "number" || inputAngka < 0){
+        return "parameter harus bertipedata nomor dan harus nomor positif";
+    } else {
+        for(let i =1; i<=10; i++){ 
+            let temp = 0;
+            temp = inputAngka * i;
+            hasil = hasil + `${inputAngka} x ${i} = ${temp} \n`;
+        }
+    }
+    return hasil;
+};
+
+module.exports = {cetakTabel} ;
+
